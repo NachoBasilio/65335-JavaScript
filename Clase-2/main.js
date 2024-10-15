d95de79b51b97c0d0a541a109cf91d51a2dfc2e0
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2024-10-14 22:23:34 -0300

    terminada la clase 2

---
+++ Clase-2/main.js
@@ -0,0 +1,113 @@
+// let numeroRandom = Math.floor((Math.random() * 10) + 1)
+
+// console.log(numeroRandom)
+
+// let numeroUsuario = Number(prompt("Hola, ingrese un numero"))
+
+
+// if(isNaN(numeroUsuario)){
+//     alert("Eso no es un numero")
+// } else {
+//     if(numeroRandom === numeroUsuario){
+//         alert("Ganaste un millon de coder points")
+//     }else if(numeroUsuario > numeroRandom){
+//         alert("Tu numero es muy grande")
+//     }else {
+//         alert("Tu numero es muy chico")
+//     }
+// }
+
+// for(let i = 0; i < 5; i++){
+//     console.log(i)
+// }
+
+// let bandera = true
+
+// while(bandera){
+//     alert("Hola, esta dentro de un while!")
+
+//     let respuesta = prompt("Mande 0 si quiere salir de este lugar")
+
+//     if(respuesta == 0){
+//         bandera = false
+//     }
+// }
+
+// console.log( typeof "Nacho" === "string" )
+
+// let respuesta;
+
+// do{
+//     respuesta = prompt("¿Cual es el numero favorito de Nacho?")
+// }while(respuesta != 6)
+
+// let op = Number(prompt("Hola, este menu es un test, pero necesito que ingrese un numero: \n 1-coca  \n 2-cola"))
+
+// switch (op) {
+//     case 1:
+//         alert("cola")
+//         break
+//     case 2:
+//         alert("coca")
+//         break
+//     default:
+//         alert("Loco, te di dos opciones y aun asi hiciste cualquiera")
+//         break;
+// }
+
+// let numeroRandom = Math.floor((Math.random() * 10) + 1)
+
+// console.log(numeroRandom)
+
+// let numeroUsuario = Number(prompt("Hola, ingrese un numero"))
+
+// while(isNaN(numeroUsuario)){
+//     alert("Eso no es un numero")
+//     numeroUsuario = Number(prompt("Hola, ingrese un numero"))
+// }
+
+
+
+//Vida
+// let contandor = 0
+
+// for( let i = 0; i < 3; i++ ){
+//     contandor++
+//     if(numeroRandom === numeroUsuario){
+//         alert("Ganaste un millon de coder points en "+ contandor + " vidas")
+//         break;
+//     }else if(numeroUsuario > numeroRandom){
+//         alert("Tu numero es muy grande")
+//     }else {
+//         alert("Tu numero es muy chico")
+//     }
+
+//     if(contandor == 3){
+//         alert("Perdiste! no pasa nada. No sos como xx, que si gano")
+//     }else{
+//         numeroUsuario = Number(prompt("Hola, ingrese un numero"))
+//     }
+// }
+
+
+
+// Intentos
+// let contandor = 0
+// let bandera = true
+
+// while(bandera){
+//     contandor++
+//     if(numeroRandom === numeroUsuario){
+//         alert("Ganaste un millon de coder points en "+ contandor + " intentos")
+//         bandera = false
+//     }else if(numeroUsuario > numeroRandom){
+//         alert("Tu numero es muy grande")
+//     }else {
+//         alert("Tu numero es muy chico")
+//     }
+//     if(bandera){
+//         numeroUsuario = Number(prompt("Hola, ingrese un numero"))
+//     }
+// }
+
+console.log(3 > 4 || "Hola" === "Holsa" )
